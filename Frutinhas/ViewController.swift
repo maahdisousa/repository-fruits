3a331c8c8616555be685cc63adb88956d0b9e152
maahdisousa <mmdisousa@gmail.com>
2022-10-13 11:09:47 -0300

    Criação do projeto no  GitHub

---
+++ Frutinhas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Frutinhas
+//
+//  Created by Marília de Sousa on 13/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+struct Fruit {
+    let name: String
+    let sugar: Double
+    let protein: Int
+    let calories: Int
+}
